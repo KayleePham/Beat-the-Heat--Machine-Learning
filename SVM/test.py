6f02cb856d2fcd123db21fec8e1a79e0eda77340
Sergio <56516024+F2Binary@users.noreply.github.com>
2021-03-22 17:13:06 -0700

    Changed to reflect modified csv

--- SVM/test.py
+++ SVM/test.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
 from sklearn.ensemble import BaggingClassifier
 #Replace with your path
-data = pd.read_csv(r"C:\Users\strai\Desktop\SVM\CA_WildFires.csv")
+data = pd.read_csv(r"C:\Users\strai\Desktop\SVM\CA_WildFires_Modified.csv")
 X = data.drop('CLASS', axis=1)
 #X = X.iloc[:,:-1].values
 X = preprocessing.scale(X)
