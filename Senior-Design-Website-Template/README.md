641f614c223118c3c35d7915aabaf9d9935a05a5
dmw01 <davidmacotoward@gmail.com>
2021-04-05 21:50:07 -0700

    Website contents with class template
    Combined template file with old index file (now index_old.html). Current issue with displaying index_old.html, doesn't load all files in dir (esp css)

---
+++ Senior-Design-Website-Template/README.md
@@ -0,0 +1,2 @@
+# Senior-Design-Website
+This is a website to showcase our senior design project which covers the scope of fire prediction using machine learning.
---
+++ Senior-Design-Website-Template/js/script.js
@@ -0,0 +1,6 @@
+function darkmode (){
+    var element = document.body;
+    var toc_title = document.getElementById("toc_title");
+    toc_title.style.color = "black";
+    element.classList.toggle("dark-theme");
+ }
